07c3864df24e17df0b2acc20bf513bb30978850f
David Walshe <david.walshe93@gmail.com>
2017-09-30 16:17:15 +0100

    Added File IO to SwingWorker

--- GUI.java
+++ GUI.java
@@ -2,7 +2,9 @@ import javafx.scene.control.ProgressBar;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import java.util.List;
 
 public class GUI implements ActionListener {
@@ -46,6 +48,16 @@ public class GUI implements ActionListener {
         txtArea.setLineWrap(true);
         txtArea.setWrapStyleWord(true);
 
+
+        // We create the ScrollPane and instantiate it with the TextArea as an argument
+        // along with two constants that define the behaviour of the scrollbars.
+        JScrollPane area = new JScrollPane(txtArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        // We then set the preferred size of the scrollpane.
+        //area.setPreferredSize(new Dimension(300, 200));
+
+
         runBtn = new JButton("Run");
         runBtn.addActionListener(this);
         cancelBtn = new JButton("Cancel");
@@ -55,13 +67,15 @@ public class GUI implements ActionListener {
         //Where the GUI is constructed:
         progressBar = new JProgressBar(0, 100);
         progressBar.setValue(0);
+        progressBar.setString("Progress: 0%");
         progressBar.setStringPainted(true);
 
 
-        totalGUI.add(txtArea, "span, push, grow");
+        totalGUI.add(area, "span, push, grow");
         totalGUI.add(progressBar, "span, grow");
         totalGUI.add(runBtn, "span, grow, split");
         totalGUI.add(cancelBtn, "span, grow, split");
+
 
         totalGUI.setOpaque(true);
         return totalGUI;
@@ -69,17 +83,16 @@ public class GUI implements ActionListener {
 
     // For the Action Events.
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == runBtn)
-        {
+        if (e.getSource() == runBtn) {
             Run("RUN");
             runBtn.setEnabled(false);
             cancelBtn.setEnabled(true);
-        }
-        else if(e.getSource() == cancelBtn)
-        {
-            Cancel("CANCEL");
+        } else if (e.getSource() == cancelBtn) {
+            worker.cancel(true);
             runBtn.setEnabled(true);
             cancelBtn.setEnabled(false);
+            progressBar.setValue(0);
+            progressBar.setString("Progress: 0%");
         }
     }
 
@@ -91,41 +104,68 @@ public class GUI implements ActionListener {
         worker = new SwingWorker<Integer, String>() {
             @Override
             protected Integer doInBackground() throws Exception {
-                publish("Iterating");
-                setProgress(0);
-                for (int i = 0; i < 10; i++) {
-                    setProgress(i*10);
-                    Thread.sleep(1000);
-                    publish("" + i);
+
+                LineNumberReader lnr = new LineNumberReader(new FileReader(new File("C:\\Users\\David\\Desktop\\Client-Server\\Assignment_1_Thread_Safe_GUI\\src\\big.txt")));
+                lnr.skip(Long.MAX_VALUE);
+                System.out.println(lnr.getLineNumber() + 1); //Add 1 because line index starts at 0
+                int lineCount = lnr.getLineNumber() + 1;
+                // Finally, the LineNumberReader object should be closed to prevent resource leak
+                lnr.close();
+
+                try ( BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Users\\David\\Desktop\\Client-Server\\Assignment_1_Thread_Safe_GUI\\src\\big.txt"))))
+                {
+                    String line;
+                    int count = 0;
+                    while((line = br.readLine()) != null)
+                    {
+                        count++;
+                        publish(line);
+                        setProgress(count/lineCount * 100);
+                    }
+                } catch ( IOException ioe )
+                {
+                    ioe.getMessage();
                 }
-                setProgress(100);
-                publish("Complete");
+
+
+//                publish("Iterating");
+//                setProgress(0);
+//                for (int i = 0; i < 10; i++) {
+//                    if (isCancelled()) {
+//                        return 0;
+//                    }
+//                    setProgress(i * 10);
+//                    Thread.sleep(1000);
+//                    publish("" + i);
+//                }
+//                setProgress(100);
+//                publish("Complete");
                 return 1;
             }
 
             @Override
             protected void process(List<String> chunks) {
-                for ( String lines :chunks ) {
+                for (String lines : chunks) {
                     txtArea.append(lines + "\r\n");
                 }
-                progressBar.setValue(worker.getProgress());
+                int prog = worker.getProgress();
+                progressBar.setValue(prog);
+                progressBar.setString("Progress: " + prog + "%");
             }
 
             @Override
             protected void done() {
                 runBtn.setEnabled(true);
                 cancelBtn.setEnabled(false);
+                if (isCancelled()) {
+                    txtArea.append("\n!! Cancelled !!\n");
+                } else
+                {
+                    txtArea.append("\n>> Done <<\n");
+                }
             }
         };
         worker.execute();
-        txtArea.setText(story);
-    }
-
-    private void Cancel(String str)
-    {
-        String story = str;
-        worker.cancel(true);
-
         txtArea.setText(story);
     }
 }
